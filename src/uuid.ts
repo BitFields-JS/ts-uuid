b16708c8a07184559ac5fd6d198d5714f7240ff8
jhaboic <josiphaboic@gmail.com>
2018-10-04 16:16:31 +0200

    Initial commit :tada:

---
+++ src/uuid.ts
@@ -0,0 +1,23 @@
+import * as crypto from 'crypto';
+
+/**
+ * Generate version 4 universally unique identifier
+ * Bytes sizes are 4, 2, 2, 2, 6 with sum of 16
+ * @returns {string}
+ */
+export const uuid16 = () => {
+  return [4, 2, 2, 2, 6].map(
+    (bin: number) => crypto.randomBytes(bin).toString('hex')
+  ).join('-');
+}
+
+/**
+ * Generate version 4 universally unique identifier.
+ * Bytes sizes are 2, 1, 1, 1, 3 with a sum of 8
+ * @returns {string}
+ */
+export const uuid8 = () => {
+  return [2, 1, 1, 1, 3].map(
+    (bin: number) => crypto.randomBytes(bin).toString('hex')
+  ).join('-');
+}
---
+++ tests/uuid.test.ts
@@ -0,0 +1,12 @@
+import { uuid16, uuid8 } from '../src/uuid';
+
+// uuid16
+test('uuid16 - return an string with length of 36 characters', () => {
+  expect(uuid16().length).toBe(36);
+});
+
+
+// uuid8
+test('uuid16 - return an string with length of 20 characters', () => {
+  expect(uuid8().length).toBe(20);
+});
